540ee36e8b6569b71774b7f7e0e446dde1dda387
alina1412 <alina1412@mail.ru>
2022-05-04 08:14:47 +0300

    edit tests

--- books_keeper/tests/test_fill_table.py
+++ books_keeper/tests/test_fill_table.py
@@ -1,51 +1,82 @@
-
 import sys
-# from typing import List
 import os
 
 address = os.environ.get("address")         # RUN IN DEBUG!!!
 sys.path.append(address)                    # type: ignore
 from collections import namedtuple          # type: ignore # noqa
-
 from book_manager import BookManager        # type: ignore # noqa
-from db_manager import SqlManager           # type: ignore # noqa
+from db_manager import SqlManager, SelectQuery      # type: ignore # noqa
 
 TABLE_NAME = "[books_002]"
 # TABLE_NAME = "[books]"
 
-def prepare(TABLE_NAME):
+
+SEL = namedtuple("SEL", ['id', "author",
+                 "title", "tags"])
+
+
+def prepare_test_bookManager(TABLE_NAME):
+    lst = [
+        SEL(id=1, author='Hans Christian Andersen',
+            title='The Ugly Duckling', tags='tale'),
+        SEL(id=2, author='Jan Ormerod', title='The Frog Prince',
+            tags='tale'),
+        SEL(id=3, author='Robert Southey',
+            title='Goldilocks and the Three Bears ', tags='tale'),
+        SEL(id=4, author='Oscar Wilde', title='The Happy Prince',
+            tags='tale'),
+        SEL(id=5, author='Michael Morpurgo', title='Hansel and Gretel',
+            tags='tale'),
+        SEL(id=6, author='Daniel Kahneman',
+            title='Thinking, Fast and Slow', tags='education, science'),
+        SEL(id=7, author='Yuval Noah Harari',
+            title='Sapiens: A Brief History of Humankind', tags='science'),
+        SEL(id=8, author='Robert Sapolski',
+            title='Zapiski Primata', tags='science')
+    ]
+
+    b_man = BookManager(TABLE_NAME)
+    for data in lst:
+        d = {"author": data[1], "title": data[2], "tags": data[3]}
+        b_man.process_query("v-add", d)
+
+
+def test_bookManager(TABLE_NAME):
+    prepare_test_bookManager(TABLE_NAME)
+
+    s_man = BookManager(TABLE_NAME)
+    test_data = {"author": "Sapolski Robert", "title": "", "tags": ""}
+    res = s_man.process_regexp_search(test_data)
+    print()
+    # print(res)
+    assert res == [SEL(id=3, author='Robert Southey',
+                   title='Goldilocks and the Three Bears ', tags='tale'),
+                   SEL(id=8, author='Robert Sapolski', title='Zapiski Primata',
+                   tags='science')]
+
+
+test_bookManager(TABLE_NAME)
+
+
+def test_SqlManager(TABLE_NAME):
     DB_init = namedtuple("DB_init", ['db_name', "table_name",
                          "column1", "column2", "column3"])
 
     init_data = DB_init("books_storage.db", TABLE_NAME,
                         "author", "title", "tags")
+    s_man = SqlManager(init_data)
+    cond = SelectQuery.get_regexp_query({"author": "ro",
+                                         "title": "", "tags": ""})
+    res = s_man.select_sql(cond)
+    # print(res)
+    assert res == [SEL(id=2, author='Jan Ormerod',
+                       title='The Frog Prince', tags='tale'),
+                   SEL(id=3, author='Robert Southey',
+                       title='Goldilocks and the Three Bears ',
+                       tags='tale'),
+                   SEL(id=8, author='Robert Sapolski',
+                       title='Zapiski Primata',
+                       tags='science')]
 
-    SEL = namedtuple("SEL", ['id', "author",
-                     "title", "tags"])
-    lst = [
-        SEL(id=1, author='Hans Christian Andersen', title='The Ugly Duckling', tags='tale'),
-        SEL(id=2, author='Jan Ormerod', title='The Frog Prince', tags='tale'),
-        SEL(id=3, author='Robert Southey', title='Goldilocks and the Three Bears ', tags='tale'),
-        SEL(id=4, author='Oscar Wilde', title='The Happy Prince', tags='tale'),
-        SEL(id=5, author='Michael Morpurgo', title='Hansel and Gretel', tags='tale'),
-        SEL(id=6, author='Daniel Kahneman', title='Thinking, Fast and Slow', tags='education, science'),
-        SEL(id=7, author='Yuval Noah Harari', title='Sapiens: A Brief History of Humankind', tags='science'),  
-        SEL(id=8, author='Robert Sapolski', title='Zapiski Primata', tags='science')
-    ]
 
-    # s_man = SqlManager(init_data)
-    b_man = BookManager(TABLE_NAME)
-    for data in lst:
-        # s_man.insert_sql(data)
-        # print(data[1])
-        d = {"author": data[1], "title": data[2], "tags": data[3]}
-        # s_man.insert_sql(tuple([*d.values()]))
-        b_man.process_query("v-add", d)
-
-prepare(TABLE_NAME)
-
-s_man = BookManager(TABLE_NAME)
-test_data = {"author": "Sapolski Robert", "title": "", "tags": ""}
-res = s_man.process_regexp_search(test_data)
-print()
-print((res))
+test_SqlManager(TABLE_NAME)
--- books_keeper/tests/test_passwords.py
+++ books_keeper/tests/test_passwords.py
@@ -1,20 +1,20 @@
 import sys
 import os
-address = os.environ.get("address")                     # RUN IN DEBUG!!!
-sys.path.append(address)                                # type: ignore
-from user_manager import PasswordLogs                 # type: ignore # noqa
+address = os.environ.get("address")               # RUN IN DEBUG!!!
+sys.path.append(address)                          # type: ignore
+from user_manager import PasswordLogs             # type: ignore # noqa
 
 
-assert PasswordLogs.validate_email("1,1@i") == False
-assert PasswordLogs.validate_email("anonymous123@...uk") == False
-assert PasswordLogs.validate_email("...@domain.us") == False
-assert PasswordLogs.validate_email("anonymous123@yahoo.co.uk") == True
-assert PasswordLogs.validate_email("name.surname@gmail.com") == True
+assert PasswordLogs.validate_email("1,1@i") is False
+assert PasswordLogs.validate_email("anonymous123@...uk") is False
+assert PasswordLogs.validate_email("...@domain.us") is False
+assert PasswordLogs.validate_email("anonymous123@yahoo.co.uk") is True
+assert PasswordLogs.validate_email("name.surname@gmail.com") is True
 
-assert PasswordLogs.validate_password("123") == False
-assert PasswordLogs.validate_password("1234567890123456789012") == False
-assert PasswordLogs.validate_password("_12fkG") == True
-assert PasswordLogs.validate_password("5fk@#") == False
-assert PasswordLogs.validate_password("sjn**") == True
+assert PasswordLogs.validate_password("123") is False
+assert PasswordLogs.validate_password("1234567890123456789012") is False
+assert PasswordLogs.validate_password("_12fkG") is True
+assert PasswordLogs.validate_password("5fk@#") is False
+assert PasswordLogs.validate_password("sjn**") is True
 
 assert type(PasswordLogs.u_hash("_12fkG")) == str
